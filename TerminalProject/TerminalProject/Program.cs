08e0095bbfaa1652a6e6137d061f92a2c8279b9b
SophiaC42 <carpes4@uw.edu>
2020-04-24 15:02:02 -0700

    E

--- TerminalProject/TerminalProject/Program.cs
+++ TerminalProject/TerminalProject/Program.cs
@@ -14,6 +14,7 @@ namespace TerminalProject
             Console.WriteLine("B");
             Console.WriteLine("C");
             Console.WriteLine("D");
+            Console.WriteLine("E");
         }
     }
 }
